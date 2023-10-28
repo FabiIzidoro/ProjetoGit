8a2c00f3e83995f23d2264844c4224564fe4c2d4
Heitor-Tasso <heitortasso12@gmail.com>
2023-10-28 15:51:41 -0300

    Problem of reset level

--- Nivel_01.py
+++ Nivel_01.py
@@ -124,24 +124,22 @@ while run:
             bg_rolar -= tela_rolar
 
         else:
-            tela_rolar = 0
-            if restart_button.draw(tela):
-                bg_rolar = 0
+            if jogador.frame_index == len(jogador.animacao_lista[jogador.acao]) - 1:
+                tela_rolar = 0
+                if restart_button.draw(tela):
+                    bg_rolar = 0
 
-            terra_data = []  # Inicialize a lista vazia
+                terra_data = []  # Inicialize a lista vazia
 
-            with open(f'nivel{nivel}_data.csv', newline='') as csvfile:
-                reader = csv.reader(csvfile, delimiter=',')
-                for linha in reader:
-                    linha_int = [int(terra) for terra in linha]
-                    terra_data.append(linha_int)
-
-
-
-
-            
-            meu_mundo = Mundo()
-            jogador, barra_vida = meu_mundo.processo_data(terra_data)
+                with open(f'nivel{nivel}_data.csv', newline='') as csvfile:
+                    reader = csv.reader(csvfile, delimiter=',')
+                    for linha in reader:
+                        linha_int = [int(terra) for terra in linha]
+                        terra_data.append(linha_int)
+                
+                meu_mundo = Mundo()
+                reiniciar_nivel()
+                jogador, barra_vida = meu_mundo.processo_data(terra_data)
 
 
         mundo.draw()
